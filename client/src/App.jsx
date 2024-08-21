971d206b65f4ddf323365992a38c2220bfded2b8
RcnonS <ratchanon.somprasong@gmail.com>
2024-08-21 15:37:47 +0700

    initialize project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=" text-red-700 text-5xl font-Montserrat font-bold ">
+        Hello 555
+      </h1>
+    </>
+  );
+}
+
+export default App;
